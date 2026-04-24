b74173923a8100330977fba65e03a474dda5db57
Ashpreet-Kaur <ashpreetkahlon70@gmail.com>
2026-04-24 12:10:59 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import Products from './components/Products'
+import ProductsPage from './pages/ProductsPages'
+
+function App() {
+  
+
+  return(
+  <>
+
+    <ProductsPage />
+  </>
+
+  )
+ 
+}
+
+export default App
---
+++ src/pages/ProductsPages.jsx
@@ -0,0 +1,15 @@
+import Products from "../components/Products"
+
+function ProductsPage(){
+    return(
+        <>
+
+            <Products />
+            <Products />
+            <Products />
+
+        </>
+    )
+}
+
+export default ProductsPage
