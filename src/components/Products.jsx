a2f1e0363ee0dc86faeeae6ba074553f5530b362
Ashpreet-Kaur <ashpreetkahlon70@gmail.com>
2026-04-24 13:29:27 +0530

    ProductPage with props added

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,12 +1,17 @@
-function Products(){
+function Products({name, description, src}){
+
+
+
+ 
+
     return(
         <>
         <div className="card" style={{width: "18rem"}} >
-        <img src="..." className="card-img-top" alt="..."/>
+        <img src={src} className="card-img-top" alt="..."/>
         <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-        <a href="#" className="btn btn-primary">Go somewhere</a>
+        <h5 className="card-title">{name}</h5>
+        <p className="card-text">{description}</p>
+        <a href="#" className="btn btn-primary">Buy {name}</a>
         </div>
 </div>
         
@@ -15,3 +20,10 @@ function Products(){
 }
 
 export default Products
+
+
+//  Props are used to pass data from the parent component to the child component. They are read-only and cannot be modified by the child component. 
+
+
+
+// The Products component here is the child component and the ProductPage is parent component as it is using the Product component inside it. The ProductPage is passing th data to the Product component through props. The Product component is receiving the data through props and using it to render the UI.
--- src/pages/ProductsPages.jsx
+++ src/pages/ProductsPages.jsx
@@ -1,15 +1,78 @@
 import Products from "../components/Products"
+import image from "../assets/hero.png"
+import "../styles/Products.css"
 
 function ProductsPage(){
+
+
+    const product1 ={
+            name: "Product1",
+            description: "This is description of product 1",
+            src : image
+    }
+
+
+    const product2 ={
+        name: "Product2",
+        description: "This is description of product 2",
+        src : image
+    }
+    
+    const product3 ={
+        name: "Product3",
+        description: "This is description of product 3",
+        src : image 
+    }
+  
+
+
     return(
         <>
 
-            <Products />
-            <Products />
-            <Products />
+        <div className="products-container">
+            <Products name={product1.name} description={product1.description} src={product1.src}/>
+            <Products name={product2.name} description={product2.description} src={product2.src}/>
+            <Products name={product3.name} description={product3.description} src={product3.src}/>
+        </div>
+
+            
 
         </>
     )
 }
 
 export default ProductsPage
+
+
+
+
+    // Destructuring is a feature in JavaScript that allows you to unpack values from arrays or properties from objects into distinct variables. It provides a more concise and readable syntax for extracting data from complex structures.
+
+//    const marks = [10,20,30,40]
+
+// //    const marks1 = marks[0]
+// //    const marks2 = marks[1]
+// //    const marks3 = marks[2]
+// //    const marks4 = marks[3]
+
+
+// const [marks1, marks2, marks3, marks4] = marks
+
+// console.log(marks1, marks2, marks3, marks4)
+
+
+// const student = {
+//     name: "John",
+//     age: 20,
+//     city: "New York"
+// }
+
+
+
+// console.log(student.name)
+// console.log(student.age)
+// console.log(student.city)
+
+
+
+// const { name, age, city } = student
